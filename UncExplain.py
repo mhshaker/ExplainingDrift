3cee56e653817dc327a93a07c2cd0e1741930b8a
mhshaker <mhshaker71@gmail.com>
2022-02-03 09:39:30 +0100

    20 episodes 100 unc_embeddings classwise high and low unc results

--- UncExplain.py
+++ UncExplain.py
@@ -9,13 +9,12 @@ import data_provider as dp
 from classes.wordclouds import Wordcloud
 from wordcloud import STOPWORDS
 
-########################################## parameters
-episodes = 100
+episodes = 20
+unc_embeddings = 100
 test_split = 0.3
 log = True
 unc_plot = False
 seed = 1
-##########################################
 
 # load the data
 features_all, targets_all, text_all = dp.load_data("./Data/")
@@ -49,26 +48,65 @@ for episode, (features, targets, texts) in enumerate(zip(features_list, targets_
         plt.savefig(f"./Results/AR_plot.png",bbox_inches='tight')
 
     # sort based on uncertianty
-    sorted_index = np.argsort(-epistemic_uncertainty, kind='stable')
+    sorted_index = np.argsort(-total_uncertainty, kind='stable')
     text_test = np.array(text_test)
     text_test = text_test[sorted_index]
-    epistemic_uncertainty = epistemic_uncertainty[sorted_index]
+    # epistemic_uncertainty = epistemic_uncertainty[sorted_index]
+
+    # Get uncertainty per class
+    u = np.unique(y_test[:,0]) # find the unique classes
+
+    tokens_high_class_list = []
+    tokens_low_class_list  = []
+    tokens_all = None
+    wordcloud = Wordcloud()
+
+    for class_value in u: # this loop runs for every class in the dataset
+        index = np.where(y_test[:,0]==class_value)[0]
+        unc_sorted_class_text = text_test[index]
+
+        wordcloud_text_array = unc_sorted_class_text[:unc_embeddings]
+        tokens_high = wordcloud.get_tokens(list(wordcloud_text_array),stopwords={})
+        tokens_high_class_list.append(tokens_high)
+
+        wordcloud_text_array = unc_sorted_class_text[-unc_embeddings:]
+        tokens_low = wordcloud.get_tokens(list(wordcloud_text_array),stopwords={})
+        tokens_low_class_list.append(tokens_low)
+
+        class_tokens = tokens_high + tokens_low
+
+        if tokens_all == None:
+            tokens_all = class_tokens
+        else:
+            tokens_all += class_tokens
+
+    for i in range(len(u)):
+        tokens = tokens_high_class_list[i]
+        for item, count in tokens.items():
+            tokens[item] /= tokens_all[item]
+        tokens_high_class_list[i] = tokens
+
+        tokens = tokens_low_class_list[i]
+        for item, count in tokens.items():
+            tokens[item] /= tokens_all[item]
+        tokens_low_class_list[i] = tokens
 
     # Create wordcloud
-
-    wordcloud_text_array = text_test[:10]
-    wordcloud = Wordcloud()
-    tokens_a = wordcloud.get_tokens(list(wordcloud_text_array))
-    cloud = wordcloud.get_wordcloud(tokens_a)
-    wordcloud.plot(cloud, f"high_epist_e{episode}")
-    if log:
-        print("High epist : ", epistemic_uncertainty[:10])
-
-    wordcloud_text_array = text_test[-10:]
-    wordcloud = Wordcloud()
-    tokens_a = wordcloud.get_tokens(list(wordcloud_text_array))
-    cloud = wordcloud.get_wordcloud(tokens_a)
-    wordcloud.plot(cloud, f"low_epist_e{episode}")
-    if log:
-        print("Low epist : ", epistemic_uncertainty[-10:])
+    fig, axs = plt.subplots(2,len(u))
+    fig.set_figheight(20)
+    fig.set_figwidth(80)
     
+    for i, class_value in enumerate(u):
+        cloud = wordcloud.get_wordcloud(tokens_high_class_list[i])
+        # axs = wordcloud.plot(cloud, f"High_class_{class_value}_e{episode}", axs=axs, axs_index=[0,i])
+        axs[0][i].imshow(cloud, interpolation="bilinear")
+        axs[0][i].axis("off")
+        axs[0][i].set_title(f"High class {class_value}")
+
+        cloud = wordcloud.get_wordcloud(tokens_low_class_list[i])
+        # axs = wordcloud.plot(cloud, f"Low_class_{class_value}_e{episode}", axs=axs, axs_index=[1,i])
+        axs[1][i].imshow(cloud, interpolation="bilinear")
+        axs[1][i].axis("off")
+        axs[1][i].set_title(f"Low class {class_value}")
+
+    fig.savefig(f"./Results/episode_{episode}.png",bbox_inches='tight') # 
